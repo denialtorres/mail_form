4559a22251b6a730535cfbc669ab2b83a42da032
denialtorres <denial.torres@gmail.com>
2025-01-18 13:05:58 -0600

    include failed test

--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
+require "fixtures/sample_mail"
 
 class MailFormTest < ActiveSupport::TestCase
   test "it has a version number" do
     assert MailForm::VERSION
+  end
+
+  test "sample mail has name and email as attributes" do
+    sample = SampleMail.new
+    sample.name = "User"
+    assert_equal "User", sample.name
+    sample.email = "user@example.com"
+    assert_equal "user@example.com", sample.email
   end
 end
