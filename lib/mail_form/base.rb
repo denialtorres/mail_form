df708600db3d9e0c248910e4d1f0d09ee742aaab
denialtorres <denial.torres@gmail.com>
2025-01-18 13:58:29 -0600

    prefix and suffix methods with ActiveModel::AttributeMethods

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,7 +1,28 @@
 module MailForm
   class Base
+    include ActiveModel::AttributeMethods # 1) attribute methds behavior
+
+    attribute_method_prefix "clear_" # 2) clear_ is attribute prefix
+    attribute_method_suffix "?" # 1) add the attribute suffix
+
     def self.attributes(*names)
       attr_accessor(*names)
+
+      # 3) ask to define the prefix methods for the given attributes names
+      define_attribute_methods(names)
+    end
+
+    protected
+
+    # 4) Since we declared a "clear_" prefix, it expects to have a
+    # "crear_attribute" method defined, which receives an attribute name and implementsthe clearing logic
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)
+    end
+
+    # 2) Implement the logic required by the "?" suffix
+    def attribute?(attribute)
+      send(attribute).present?
     end
   end
 end
